d67eb3923ed677e5e32ad411b5da2bb6f3698e14
Serge Nahumenko <spit437@gmail.com>
2016-08-22 22:50:23 +0300

    Добавил в калькулятор горючих газов Ацетилен

--- master/PtoCalc.Designer.cs
+++ master/PtoCalc.Designer.cs
@@ -34,9 +34,16 @@
             this.nUpDownMAF = new System.Windows.Forms.NumericUpDown();
             this.lblProp = new System.Windows.Forms.Label();
             this.lblMAF = new System.Windows.Forms.Label();
+            this.lblAcetelen = new System.Windows.Forms.Label();
+            this.nUpDownAcetelen = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.nUpDownPropan)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.nUpDownMAF)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nUpDownAcetelen)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -45,7 +52,7 @@
             this.подсказкаToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(567, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(543, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -116,24 +123,106 @@
             this.lblMAF.TabIndex = 4;
             this.lblMAF.Text = "МАФ";
             // 
+            // lblAcetelen
+            // 
+            this.lblAcetelen.AutoSize = true;
+            this.lblAcetelen.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblAcetelen.Location = new System.Drawing.Point(363, 64);
+            this.lblAcetelen.Name = "lblAcetelen";
+            this.lblAcetelen.Size = new System.Drawing.Size(98, 24);
+            this.lblAcetelen.TabIndex = 6;
+            this.lblAcetelen.Text = "Ацетелен";
+            // 
+            // nUpDownAcetelen
+            // 
+            this.nUpDownAcetelen.DecimalPlaces = 3;
+            this.nUpDownAcetelen.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.nUpDownAcetelen.Location = new System.Drawing.Point(354, 91);
+            this.nUpDownAcetelen.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.nUpDownAcetelen.Name = "nUpDownAcetelen";
+            this.nUpDownAcetelen.Size = new System.Drawing.Size(120, 31);
+            this.nUpDownAcetelen.TabIndex = 5;
+            this.nUpDownAcetelen.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            327680});
+            this.nUpDownAcetelen.ValueChanged += new System.EventHandler(this.nUpDownAcetelen_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(24, 201);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(473, 62);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "1 [кг] Пропан-бутан = 0,613 [кг] МАФ\r\n1 [м3] Ацетилен= 0,613 [кг] МАФ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.Chartreuse;
+            this.label2.Location = new System.Drawing.Point(59, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 25);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "КГ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.Chartreuse;
+            this.label3.Location = new System.Drawing.Point(219, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 25);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "КГ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(381, 125);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 25);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "М3\r\n";
+            // 
             // PtoCalc
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(567, 336);
+            this.ClientSize = new System.Drawing.Size(543, 300);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblAcetelen);
+            this.Controls.Add(this.nUpDownAcetelen);
             this.Controls.Add(this.lblMAF);
             this.Controls.Add(this.lblProp);
             this.Controls.Add(this.nUpDownMAF);
             this.Controls.Add(this.nUpDownPropan);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
+            this.MaximumSize = new System.Drawing.Size(559, 339);
+            this.MinimumSize = new System.Drawing.Size(559, 339);
             this.Name = "PtoCalc";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Калькулятор ПТО";
+            this.Text = "Калькулятор горючих газов";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.nUpDownPropan)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.nUpDownMAF)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nUpDownAcetelen)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -147,5 +236,11 @@
         private System.Windows.Forms.NumericUpDown nUpDownMAF;
         private System.Windows.Forms.Label lblProp;
         private System.Windows.Forms.Label lblMAF;
+        private System.Windows.Forms.Label lblAcetelen;
+        private System.Windows.Forms.NumericUpDown nUpDownAcetelen;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
--- master/PtoCalc.cs
+++ master/PtoCalc.cs
@@ -12,6 +12,7 @@ namespace master
     {
         double mafValue;// значения мафа
         double propButValue; // значение пропан бутана 
+        double acetilValue; // значение ацетелена
 
         public PtoCalc()
         {
@@ -21,25 +22,12 @@ namespace master
 
         private void nUpDownPropan_ValueChanged(object sender, EventArgs e)
         {
-            if (nUpDownPropan.Focused==true)
-            {
-                getValue();
-                mafValue = propButValue * 0.613;
-                nUpDownMAF.Value = Convert.ToDecimal(mafValue);
-            }
-
-
+            returnValue();
         }
 
         private void nUpDownMAF_ValueChanged(object sender, EventArgs e)
         {
-            if (nUpDownMAF.Focused==true)
-            {
-                getValue();
-                propButValue = mafValue * 1.63;
-                nUpDownPropan.Value = Convert.ToDecimal(propButValue);
-
-            }
+            returnValue();
         }
 
         // получить значения 
@@ -47,13 +35,50 @@ namespace master
         {
             mafValue = Convert.ToDouble(nUpDownMAF.Value);
             propButValue = Convert.ToDouble(nUpDownPropan.Value);
-            
+            acetilValue = Convert.ToDouble(nUpDownAcetelen.Value);
+        }
+        //печать значений
+        public void printValue()
+        {
+            nUpDownPropan.Value = Convert.ToDecimal(propButValue);
+            nUpDownAcetelen.Value = Convert.ToDecimal(acetilValue);
+            nUpDownMAF.Value = Convert.ToDecimal(mafValue);
+        }
 
+
+        // в зависимости от смены нумерика
+        public void returnValue()
+        {
+            getValue();
+            // MAF
+            if (nUpDownMAF.Focused == true)
+            {
+                propButValue = mafValue * 1.63;
+                acetilValue = mafValue * 0.943;
+            }
+            //Propan
+            if (nUpDownPropan.Focused == true)
+            {
+                mafValue = propButValue * 0.613;
+                acetilValue = mafValue * 0.943;
+            }
+            // Acetilen
+            if (nUpDownAcetelen.Focused == true)
+            {
+                mafValue = acetilValue * 1.06;
+                propButValue = mafValue * 1.63;
+            }
+            printValue();
         }
 
         private void подсказкаToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Все работает по щелчку.\n Единица измерения килограммы.", "подсказка");
+            MessageBox.Show("Все работает по щелчку.", "подсказка");
+        }
+
+        private void nUpDownAcetelen_ValueChanged(object sender, EventArgs e)
+        {
+            returnValue();
         }
     }
 
